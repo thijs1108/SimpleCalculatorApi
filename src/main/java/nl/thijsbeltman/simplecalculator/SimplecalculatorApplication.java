742f8564aad7cb5706af9f2ed6b98ae9f29f637d
tbeltman <tbeltman@quintor.nl>
2020-12-21 08:48:30 +0100

    init

---
+++ src/main/java/nl/thijsbeltman/simplecalculator/SimplecalculatorApplication.java
@@ -0,0 +1,13 @@
+package nl.thijsbeltman.simplecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplecalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/thijsbeltman/simplecalculator/SimplecalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.thijsbeltman.simplecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
